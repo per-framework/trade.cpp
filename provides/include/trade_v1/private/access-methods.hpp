4e301e9b0d78d81ddebc188efc5313b1df033e5a
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-09-07 00:42:05 +0300

    Fix bug with ref after load of type with non-trivial dtor

--- provides/include/trade_v1/private/access-methods.hpp
+++ provides/include/trade_v1/private/access-methods.hpp
@@ -3,7 +3,12 @@
 #include "trade_v1/private/access.hpp"
 
 template <class Value>
-void trade_v1::Private::access_t<Value, false>::retain() {
+void trade_v1::Private::access_t<Value, false>::retain_copy() {
+  new (&m_original) Value(m_current);
+}
+
+template <class Value>
+void trade_v1::Private::access_t<Value, false>::retain_move() {
   new (&m_original) Value(std::move(m_current));
 }
 
@@ -17,7 +22,10 @@ void trade_v1::Private::access_t<Value, false>::destroy() {
 }
 
 template <class Value>
-void trade_v1::Private::access_t<Value, true>::retain() {}
+void trade_v1::Private::access_t<Value, true>::retain_copy() {}
+
+template <class Value>
+void trade_v1::Private::access_t<Value, true>::retain_move() {}
 
 template <class Value>
 void trade_v1::Private::access_t<Value, true>::destroy() {}
